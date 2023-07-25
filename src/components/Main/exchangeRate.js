5060a44bc8e1086e4ceaab56b16820f497f92644
Rizabetta <liza.popechiteleva@mail.ru>
2023-07-26 02:15:21 +0400

    Enabling the current course display service

---
+++ src/components/Main/exchangeRate.js
@@ -0,0 +1,21 @@
+let key = new XMLHttpRequest();
+key.open('GET', "../src/keys/rapidAPI.txt", false);
+key.send();
+
+let xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener('readystatechange', function () {
+    if (this.readyState === this.DONE) {
+    }
+});
+
+let exchange = ['USD', 'EUR', 'CNY', 'TRY', 'CHF', 'JPY'];
+exchange.forEach(item => {
+    xhr.open('GET', `https://currency-exchange.p.rapidapi.com/exchange?from=${item}&to=RUB&q=1`, false);
+    xhr.setRequestHeader('X-RapidAPI-Key', `${key.responseText}`);
+    xhr.setRequestHeader('X-RapidAPI-Host', 'currency-exchange.p.rapidapi.com');
+    xhr.send();
+    document.getElementById(item).innerHTML = Number(xhr.responseText).toFixed(2);
+    console.log(Number(xhr.responseText).toFixed(2));
+})
