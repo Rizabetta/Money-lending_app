24b01c5ea1577cfde4a1e0b854e6da57e80cae1c
Rizabetta <liza.popechiteleva@mail.ru>
2023-07-29 04:23:42 +0400

    hover buttons and media queries

--- src/components/Main/news.js
+++ src/components/Main/news.js
@@ -2,14 +2,14 @@ let newsKey = new XMLHttpRequest();
 newsKey.open('GET', "../src/keys/newsAPI.txt", false);
 newsKey.send();
 
-var url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${newsKey.responseText}`;
-var req = new Request(url);
+let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${newsKey.responseText}`;
+let req = new Request(url);
 
 fetch(req)
     .then(response => response.json()).then(articles => {
+        let arr = 0;
         for (i = 0; i < articles.articles.length; i++) {
-
-            if (articles.articles[i].urlToImage == null)
+            if (!articles.articles[i].urlToImage)
                 console.log('null'); else {
                 let div = document.createElement("div");
                 div.className = "news__card";
@@ -34,25 +34,50 @@ fetch(req)
                 description.innerHTML = articles.articles[i].description;
                 description.className = "card__description";
                 a.append(description);
+                arr++;
             }
         }
+        test(arr);
     });
 
 let position = 0;
-let trackWidth = document.getElementsByClassName('news__track')[0].clientWidth;
+let gap = 80;
+let cardkWidth = 320;
+let move = gap + cardkWidth;
 let track = document.querySelector('.news__track');
 
-// 320px + 80px = 400px
-// 6400  = 320*20
-function leftButton() {
-    console.log('Ширина:', trackWidth);
-    if (position !== 0) position += 400 ;
+function carouselLeftBtn() {
+    if (position !== 0) position += move;
     track.style.transform = `translateX(${position}px)`;
+    document.getElementsByClassName("news__input-left").src = "../public/assets/svg/ButtonR.svg";
 }
 
-function rightButton() {
+function carouselRightBtn() { 
     if (position !== -6400)
-    position -= 400;
+        position -= move;
     track.style.transform = `translateX(${position}px)`;
-    console.log(position);
+    document.getElementsByClassName("news__input-right").src = "../public/assets/svg/ButtonR.svg";
+}
+
+function test(arr) {
+    let trackWidth = document.getElementsByClassName('news__track')[0].clientWidth;
+    let visibleElements = 0;
+    if ((trackWidth == 1300) || (trackWidth == 1160)) visibleElements = 3;
+    if (trackWidth == 770) visibleElements = 2;
+    if (trackWidth == 350) visibleElements = 1;
+    // arr - visibleElements = 18 - 3 = 15 => 15 * 400 = 6000
+    let hover = document.querySelectorAll(".carouselBtn");
+    let currentImage;
+    for (let i = 0; i < hover.length; i++) {
+        hover[i].addEventListener("mouseover", function () {
+            currentImage = this.src;
+            this.src = "../public/assets/svg/ButtonR.svg";
+        });
+        hover[i].addEventListener("mouseout", function () {
+            this.src = currentImage;
+        });
+    }
+    var a = Number(arr - visibleElements) * 400;
+    console.log(Number(arr - visibleElements) * 400);
+    console.log(arr);
 }
