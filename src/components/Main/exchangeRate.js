30ca11625ad1bef10d99fb390b41a63cd2f6df2b
Rizabetta <liza.popechiteleva@mail.ru>
2023-08-01 21:17:27 +0400

    separating fetch into an API file

--- src/components/Main/exchangeRate.js
+++ src/components/Main/exchangeRate.js
@@ -1,3 +1,5 @@
+import { fetchCurrency } from "../../API/api.js"
+
 const mseconds = 1000;
 const seconds = 60;
 const minutes = 15;
@@ -12,23 +14,11 @@ const exchange = [
   { from: 'JPY', to: 'RUB' },
 ]
 
-function fetchCurrency(from, to) {
-  const url = `https://currency-exchange.p.rapidapi.com/exchange?from=${from}&to=${to}&q=1.0`;
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '29195502fbmsh29ef30de2b5d032p1b9633jsn9a396d0b7e87',
-      'X-RapidAPI-Host': 'currency-exchange.p.rapidapi.com'
-    }
-  };
-  const response = fetch(url, options).then((response) =>  response.text()).then(data => document.getElementById(from).innerHTML = Number(data).toFixed(2));
-}
-
-function  update() {
+function update() {
   exchange.forEach(element => {
-    fetchCurrency(element.from, element.to);
+    fetchCurrency(element.from, element.to).then(data => document.getElementById(element.from).innerHTML = Number(data).toFixed(2));
   });
-  setInterval(()=>update(),updateRequest)
+  setInterval(() => update(), updateRequest)
 }
 
 update();
