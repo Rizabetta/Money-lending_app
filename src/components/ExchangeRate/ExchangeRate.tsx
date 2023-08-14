3579c6551637fa8dcfc94f8b8f6dbbc3f4ae0306
Rizabetta <liza.popechiteleva@mail.ru>
2023-08-14 04:20:57 +0400

    automatic code formatting

--- src/components/ExchangeRate/ExchangeRate.tsx
+++ src/components/ExchangeRate/ExchangeRate.tsx
@@ -1,41 +1,48 @@
-import './ExchangeRate.scss';
-import pantheon from '../../assets/svg/Pantheon.svg'; 
+import "./ExchangeRate.scss";
+import pantheon from "../../assets/svg/Pantheon.svg";
 
 const rateTable1 = [
-    {index:1, rate: "USD:", id: "USD"},
-    {index:2, rate: "CNY:", id: "CNY"},
-    {index:3, rate: "CHF:", id: "CHF"},
-]
+  { index: 1, rate: "USD:", id: "USD" },
+  { index: 2, rate: "CNY:", id: "CNY" },
+  { index: 3, rate: "CHF:", id: "CHF" },
+];
 const rateTable2 = [
-    {index:1, rate: "EUR:", id: "EUR"},
-    {index:2, rate: "JPY:", id: "JPY"},
-    {index:3, rate: "TRY:", id: "TRY"},
-]
+  { index: 1, rate: "EUR:", id: "EUR" },
+  { index: 2, rate: "JPY:", id: "JPY" },
+  { index: 3, rate: "TRY:", id: "TRY" },
+];
 export default function ExchangeRate() {
-    return (
-        <section className="exchangeRate">
-        <div>
-            <h3>Exchange rate in internet bank</h3>
-            <p className="exchangeRate__currency-p">Currency</p>
-            <ul className="exchangeRate__currency-table">
-                {rateTable1.map(item => <div key={item.index}>
-                <li className="exchangeRate__currency">{item.rate}</li>
-                <li className="exchangeRate__value" id={item.id}></li>
-                </div>)}
-            </ul>
-            <ul className="exchangeRate__currency-table">
-            {rateTable2.map(item => <div key={item.index}> 
-                <li className="exchangeRate__currency">{item.rate}</li>
-                <li className="exchangeRate__value" id={item.id}></li>
-                </div>)}   
-            </ul>
-            <a href="# " className="exchangeRate__courses-a">All courses</a>
-        </div>
-        <div className="exchangeRate__container">
-            <time className="exchangeRate__time">Update every 15 minutes, MSC 09.08.2022</time>
-            <img src={pantheon} alt="Pantheon" />
-        </div>
+  return (
+    <section className="exchangeRate">
+      <div>
+        <h3>Exchange rate in internet bank</h3>
+        <p className="exchangeRate__currency-p">Currency</p>
+        <ul className="exchangeRate__currency-table">
+          {rateTable1.map((item) => (
+            <div key={item.index}>
+              <li className="exchangeRate__currency">{item.rate}</li>
+              <li className="exchangeRate__value" id={item.id}></li>
+            </div>
+          ))}
+        </ul>
+        <ul className="exchangeRate__currency-table">
+          {rateTable2.map((item) => (
+            <div key={item.index}>
+              <li className="exchangeRate__currency">{item.rate}</li>
+              <li className="exchangeRate__value" id={item.id}></li>
+            </div>
+          ))}
+        </ul>
+        <a href="# " className="exchangeRate__courses-a">
+          All courses
+        </a>
+      </div>
+      <div className="exchangeRate__container">
+        <time className="exchangeRate__time">
+          Update every 15 minutes, MSC 09.08.2022
+        </time>
+        <img src={pantheon} alt="Pantheon" />
+      </div>
     </section>
-    );
-  }
-  
+  );
+}
--- src/components/PlatinumCard/PlatinumCard.tsx
+++ src/components/PlatinumCard/PlatinumCard.tsx
@@ -1,35 +1,43 @@
-import imgCard from '../../assets/svg/PlatinumCard.svg';
-import './PlatinumCard.scss';
+import imgCard from "../../assets/svg/PlatinumCard.svg";
+import "./PlatinumCard.scss";
 
 export default function PlatinumCard() {
-    return (
-          <section className='platinumCard'>
-            <div className='platinumCard__description'>
-                <h1>Platinum digital credit card</h1>
-                <p>Our best credit card. Suitable for everyday spending and shopping.
-                    Cash withdrawals and transfers without commission and interest.</p>
-                <div className='platinumCard__columns'>
-                    <ul className='tooltip'>
-                        <li>Up to 160 days</li>
-                        <li>No percent</li>
-                        <span className="tooltiptext">When repaying the full debt up to 160 days.</span>
-                    </ul>
-                    <ul className='tooltip'>
-                        <li> Up to 600 000 &#8381;</li>
-                        <li>Credit limit</li>
-                        <span className="tooltiptext">Over the limit willaccrue percent</span>
-                    </ul>
-                    <ul className='tooltip'>
-                        <li>0 &#8381;</li>
-                        <li>Card service is free</li>
-                        <span className="tooltiptext">Promotion valid until December 31, 2022.</span>
-                    </ul>
-                </div>
-                <button className="defaultButton">Apply for card</button>
-            </div>
-            <div className='platinumCard__img'>
-                <img src={imgCard} alt='Platinum Card'></img>
-            </div>
-          </section>
-    );
+  return (
+    <section className="platinumCard">
+      <div className="platinumCard__description">
+        <h1>Platinum digital credit card</h1>
+        <p>
+          Our best credit card. Suitable for everyday spending and shopping.
+          Cash withdrawals and transfers without commission and interest.
+        </p>
+        <div className="platinumCard__columns">
+          <ul className="tooltip">
+            <li>Up to 160 days</li>
+            <li>No percent</li>
+            <span className="tooltiptext">
+              When repaying the full debt up to 160 days.
+            </span>
+          </ul>
+          <ul className="tooltip">
+            <li> Up to 600 000 &#8381;</li>
+            <li>Credit limit</li>
+            <span className="tooltiptext">
+              Over the limit willaccrue percent
+            </span>
+          </ul>
+          <ul className="tooltip">
+            <li>0 &#8381;</li>
+            <li>Card service is free</li>
+            <span className="tooltiptext">
+              Promotion valid until December 31, 2022.
+            </span>
+          </ul>
+        </div>
+        <button className="defaultButton">Apply for card</button>
+      </div>
+      <div className="platinumCard__img">
+        <img src={imgCard} alt="Platinum Card"></img>
+      </div>
+    </section>
+  );
 }
