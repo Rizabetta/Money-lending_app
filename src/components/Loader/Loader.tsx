9b7cfe704dacdaed84f75fd9428d2e3e26745649
Rizabetta <liza.popechiteleva@mail.ru>
2023-08-20 18:24:19 +0400

    new Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+import "./Loader.scss";
+
+export default function Loader() {
+  return (
+    <div className="loader__container">
+      <div className="loader__loading"></div>
+    </div>
+  );
+}
