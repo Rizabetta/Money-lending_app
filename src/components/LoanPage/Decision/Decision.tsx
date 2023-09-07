8d226e1555b929f1049f8b846312d8a7b54575e3
Rizabetta <liza.popechiteleva@mail.ru>
2023-09-08 01:09:49 +0400

    refactor: create component Decision

---
+++ src/components/LoanPage/Decision/Decision.tsx
@@ -0,0 +1,15 @@
+import "./Decision.scss";
+
+function Decision() {
+  return (
+    <section className="decision">
+      <h3>The preliminary decision has been sent to your email.</h3>
+      <p>
+        In the letter you can get acquainted with the preliminary decision on
+        the credit card.
+      </p>
+    </section>
+  );
+}
+
+export { Decision };
--- src/components/LoanPage/index.ts
+++ src/components/LoanPage/index.ts
@@ -1,3 +1,4 @@
+export { Decision } from "./Decision";
 export { InstructionGetCart } from "./InstructionGetCart";
 export { PlatinumCard } from "./PlatinumCard";
 export { Prescoring } from "./Prescoring";
