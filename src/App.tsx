a89546b1106a6cf98bac85d6c86f300565653cc9
Rizabetta <liza.popechiteleva@mail.ru>
2023-09-21 04:14:38 +0400

    refactor: remov unused dependencies, cleaning code

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React, { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
-import "./scss/normalize.css";
-import "./scss/index.scss";
 import { Footer, Header } from "./layout";
 import { Routers } from "./components";
 import { Loader } from "./components/UI";
+import "./scss/normalize.css";
+import "./scss/index.scss";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Suspense fallback={<Loader/>}>
+      <Suspense fallback={<Loader />}>
         <Routers />
       </Suspense>
       <Footer />
